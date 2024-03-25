fde9d43b6f8a62c50d28aa1fdcd02dacd9fa7901
Kostiantyn <zvieriev.kp31.ms@gmail.com>
2024-03-25 16:27:57 +0200

    +: create functions for writing and reading data from files

---
+++ practice_1/file.ts
@@ -0,0 +1,29 @@
+import { writeFileSync, readFileSync } from 'fs';
+
+export const write_to_file = function (path: string, content: string) {
+    try {
+        writeFileSync(path, content);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const read_from_file = function (path: string) {
+    try {
+        const data = readFileSync(path, 'utf8');
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export const load_from_file = function (path: string) {
+    const data = read_from_file(path) as string;
+    const array = data.split('\n').map((sub_arr: string) => {
+        const arr_string = sub_arr.trim();
+        if(arr_string.length > 0){
+            return sub_arr.split('').map(Number);
+        }
+    })
+    return array.filter(item => item);
+}
